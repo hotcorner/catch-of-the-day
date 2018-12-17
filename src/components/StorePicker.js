85f2ae8289a3844fe47f0040a9e95046adca741b
Clint Rowden <crowden@ea.com>
2018-12-17 12:51:17 -0500

    added CSS import

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { format } from 'url';
 
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className="store-selctor">
+      <form className="store-selector">
         <h2>Please enter a Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="Submit">Visit Store</button>
