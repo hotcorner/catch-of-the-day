964a21fe21380ad32e1dbeaf6bfed0c16ff1c271
Clint Rowden <crowden@ea.com>
2018-12-19 15:07:59 -0500

    made StorePicker stateless, functional component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-class StorePicker extends React.Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
-        <button type="Submit">Visit Store</button>
-      </form>
-    )
-  }
-}
+const StorePicker = () => (
+  <form className="store-selector">
+    <h2>Please enter a Store</h2>
+    <input type="text" required placeholder="Store Name" />
+    <button type="Submit">Visit Store</button>
+  </form>
+)
+
 
 export default StorePicker;
