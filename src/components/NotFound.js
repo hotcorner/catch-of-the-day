7577f5a4d56cee1def5752a40d9147f5957e8ffd
Clint Rowden <crowden@ea.com>
2018-12-19 15:24:01 -0500

    added styling to NotFound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const NotFound = () => ( <div><h2>NOT FOUND</h2></div>);
+const NotFound = () => ( <div className="store-selector"><h2>NOT FOUND</h2></div>);
 
 export default NotFound;
