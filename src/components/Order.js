c19da2fd133310543368fe284ec63ade58e96821
Clint <clintonia@gmail.com>
2019-01-07 11:46:20 -0500

    finished animations

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,27 +7,41 @@ class Order extends React.Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === "available";
+    const orderAnimationOptions = {
+      classNames: "order",
+      key,
+      timeout: { enter: 500, exit: 500 }
+    };
     if (!fish) return null;
     if (!isAvailable) {
       return (
-        <li key={key}>
-          Sorry, {fish && fish.name !== "" ? fish.name : "this fish"} is sold
-          out.
-        </li>
+        <CSSTransition {...orderAnimationOptions}>
+          <li key={key}>
+            Sorry, {fish && fish.name !== "" ? fish.name : "this fish"} is sold
+            out.
+          </li>
+        </CSSTransition>
       );
     }
     return (
-      <CSSTransition
-        classNames="order"
-        key={key}
-        timeout={{ enter: 250, exit: 250 }}
-      >
+      <CSSTransition {...orderAnimationOptions}>
         <li key={key}>
-          {count}lbs {fish.name}
-          {formatPrice(count * fish.price)}
-          <button onClick={() => this.props.removeFromOrder(key)}>
-            &times;
-          </button>
+          <span>
+            <TransitionGroup component="span" className="count">
+              <CSSTransition
+                classNames="count"
+                key={count}
+                timeout={orderAnimationOptions.timeout}
+              >
+                <span>{count}</span>
+              </CSSTransition>
+            </TransitionGroup>
+            lbs {fish.name}
+            {formatPrice(count * fish.price)}
+            <button onClick={() => this.props.removeFromOrder(key)}>
+              &times;
+            </button>
+          </span>
         </li>
       </CSSTransition>
     );
